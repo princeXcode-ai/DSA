b7a7a3acdee1301b00ff97b87450ef2bea2d15af
princeXcode-ai <princekushwahap09@gmail.com>
2026-09-08 23:42:23 +0530

    learning java

---
+++ Methods/BuiltIn.java
@@ -0,0 +1,12 @@
+public class BuiltIn {
+    public static void main(String[] args) {
+        // System.out.println(Math.sqrt(9.8));
+        // System.out.println(Math.cbrt(100));
+        // System.out.println(Math.abs(-89));
+        // System.out.println(Math.floor(-7.8));
+        // System.out.println(Math.ceil(-7.8));
+        // System.out.println(Math.min(-7.8,5));
+        // System.out.println(Math.max(-7.8,7));
+        System.out.println(Math.pow(-7.8,7));
+    }
+}
---
+++ Methods/Syntax.java
@@ -0,0 +1,15 @@
+public class Syntax {
+    public static void Prince(){
+        System.out.println("Hero");
+
+    }
+    public static void main(String[] args) {
+        System.out.println("Roshan");
+        Prince();
+        Roshan();
+    }
+    public static void Roshan(){
+        System.out.println("Prince");
+
+    }
+}
